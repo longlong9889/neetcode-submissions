7b769a7e3630768f267279bc44b1fe76a0e5b511
longlong9889 <longhuyhoangnguyen@csus.edu>
2026-07-15 22:18:20 -0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(')');
+            }
+            else if (s.charAt(i) == '[') {
+                stack.push(']');
+            }
+            else if (s.charAt(i) == '{') {
+                stack.push('}');
+            }
+            else {
+                if (!stack.isEmpty() && s.charAt(i) == stack.peek()) {
+                    stack.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        if (stack.isEmpty()) {
+            return true;
+        }
+        else return false;
+    }
+}

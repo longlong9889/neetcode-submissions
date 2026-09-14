674a5741ac44501146188cd5397e28edd860b7d5
longlong9889 <longhuyhoangnguyen@csus.edu>
2026-09-14 11:10:59 -0700

    Add: longest-increasing-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 1) {
+            return 1;
+        }
+        int longest = 1;
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = 1;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i] && dp[i] <= dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            if (dp[i] > longest) {
+                longest = dp[i];
+            }
+        }
+        return longest;
+    }
+}
